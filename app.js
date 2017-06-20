355e37276951d06c99e3e1df753196ca77b93a97
yuzhuo li <yuzhuoli@yuzhuos-MacBook-Pro.local>
2017-06-20 16:45:07 +0800

    add package json configuration files

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var routes = require("./routes/routes.js")(app);
 
-var server = app.listen(3000, function () {
-    console.log("Listening on port %s...", server.address().port);
+var server = app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
