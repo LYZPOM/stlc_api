3928e01d1e843352718d26fda8b6147d52d908ce
yuzhuo li <yuzhuoli@yuzhuos-MacBook-Pro.local>
2017-06-21 23:43:49 +0800

    update json data

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
-app.set('port', (process.env.PORT || 80));
+app.set('port', (process.env.PORT || 3000));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
